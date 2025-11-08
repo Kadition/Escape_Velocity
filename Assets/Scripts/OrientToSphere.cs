3217e46141d3a1bced4a292679ff68d46c792302
Jonas Beckham <jcbeckham42@gmail.com>
2025-11-08 01:08:23 -0600

    Planet Cam
    the camera.down now points to the nearest planetary body, player body also rotates so that transform.down is pointing to the nearest planet

---
+++ Assets/Scripts/OrientToSphere.cs
@@ -0,0 +1,45 @@
+
+using UnityEngine;
+
+//attached to the camera gameobject
+// this script ensures that the camera follows the player as they walk across the surface of a sphere.
+public class OrientToSphere : MonoBehaviour
+{
+
+    [SerializeField] Vector3 planetPos; // The position of the nearest planet
+    [SerializeField] Vector3 playerToPlanetVector; // Vector from player to nearest planet
+    [SerializeField] GameObject playerCenter; // the player gameobject
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        planetPos = GetNearestPlanet().transform.position;
+        playerToPlanetVector = planetPos - playerCenter.transform.position;
+        //playerToPlanetVector.z += 90; //offset to keep camera in line with global z axis
+        transform.up = -playerToPlanetVector.normalized; // Set the camera's up direction to be away from the center of the current planet
+    
+    }
+
+    private GameObject GetNearestPlanet()
+    {
+
+        GameObject[] planets = GameObject.FindGameObjectsWithTag("Planet");
+        float leastMag = Mathf.Infinity; //least magnitude between player and planets
+        GameObject nearestPlanet = null;
+        foreach (GameObject planet in planets)
+        {
+            if ((playerCenter.transform.position - planet.transform.position).magnitude < leastMag)
+            {
+                leastMag = (playerCenter.transform.position - planet.transform.position).magnitude;
+                nearestPlanet = planet; 
+            }
+        }
+
+        return nearestPlanet;
+    }
+
+}
