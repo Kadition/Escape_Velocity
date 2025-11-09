53b559d930cf49e2b0382913b0b232ed8ca65b95
Jonas Beckham <jcbeckham42@gmail.com>
2025-11-08 18:08:12 -0600

    Volume Slider

--- Assets/Scripts/SoundFXManager.cs
+++ Assets/Scripts/SoundFXManager.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundFXManager : MonoBehaviour
 {
     [SerializeField] private AudioSource soundFXObject;
+    [SerializeField] private float globalVolume = 1.0f;
     public static SoundFXManager instance;
+    [SerializeField] private Slider volumeSlider;
 
     private void Awake()
     {
@@ -13,12 +16,25 @@ public class SoundFXManager : MonoBehaviour
         }
     } 
 
+    private void Start()
+    {
+        if (volumeSlider != null)
+        {
+            volumeSlider.onValueChanged.AddListener((v) => SetGlobalVolume(v));
+        }
+    }
+
+    public void SetGlobalVolume(float volume)
+    {
+        globalVolume = volume;
+    }
+
     public void PlaySoundFXCLip(AudioClip clip, Transform spawnTransform, float volume = 1.0f)
     {
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
 
         audioSource.clip = clip;
-        audioSource.volume = volume;
+        audioSource.volume = volume * globalVolume;
         audioSource.Play();
         float clipLength = audioSource.clip.length;
         Destroy(audioSource.gameObject, clipLength);
