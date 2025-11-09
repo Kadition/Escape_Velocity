515dd2827d57d9673c67fa3b7055dcb8ba857688
Jonas Beckham <jcbeckham42@gmail.com>
2025-11-08 23:53:21 -0600

    Updated ZeroGTimer
    fixed StartCoroutine errors
    added red filter to view

--- Assets/Scripts/ZeroGTimer.cs
+++ Assets/Scripts/ZeroGTimer.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using System.Collections;
 //attached to the player game object
 // this script will respawn the player if they are in zero gravity for too long
@@ -31,4 +31,78 @@ public class ZeroGTimer : MonoBehaviour
         yield return new WaitForSeconds(timerLength);
         GetComponent<PlayerRespawn>().RespawnPlayer();
     }
+}*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ZeroGTimer : MonoBehaviour
+{
+    PlayerController pC;
+    [SerializeField] float timerLength = 10f;
+    Coroutine deathCoroutine;
+    [SerializeField] Canvas redCanvas;
+    [SerializeField] Image redImage;
+
+    void Start()
+    {
+        pC = gameObject.GetComponent<PlayerController>();
+        redCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (pC.gravityVector == pC.NormGravity)
+        {
+            if (deathCoroutine == null)
+            {
+                deathCoroutine = StartCoroutine(DeathTimer());
+
+            }
+        }
+        else
+        {
+            if (deathCoroutine != null)
+            {
+                StopCoroutine(deathCoroutine);
+                deathCoroutine = null;
+                ResetRedFade();
+            }
+        }
+    }
+
+    IEnumerator DeathTimer()
+    {
+        StartCoroutine(FadeToRed());
+        yield return new WaitForSeconds(timerLength);
+        GetComponent<PlayerRespawn>().RespawnPlayer();
+        ResetRedFade();
+        deathCoroutine = null; // reset after completion
+    }
+
+    IEnumerator FadeToRed()
+    {
+        redCanvas.enabled = true;
+        Color color = redImage.color;
+        color.a = 0;
+        // Fade to black
+        while (color.a < 0.5)
+        {
+            color.a += 0.001f;
+            redImage.color = color;
+            yield return null;
+        }
+    }
+    
+    public void ResetRedFade()
+    {
+        // jump back to transparent
+        StopCoroutine(FadeToRed());
+        Color color = redImage.color;
+        color.a = 0;
+        redImage.color = color;
+        redCanvas.enabled = false;
+    }
 }
+
